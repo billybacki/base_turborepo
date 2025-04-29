f01bd4698b389c4e0663a1366ba8f201a75d5aa2
william <>
2025-04-29 16:27:57 +0800

    feat: add connect button for example next

--- example/next/app/page.tsx
+++ example/next/app/page.tsx
@@ -1,9 +1,15 @@
+'use client'
 import Image from 'next/image'
+import { useAppKit, useAppKitAccount } from '@reown/appkit/react'
 
 export default function Home() {
+  const { open } = useAppKit()
+  const { address } = useAppKitAccount()
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        {!address ? <button onClick={() => open()}>Connect</button> : <p>{address}</p>}
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
